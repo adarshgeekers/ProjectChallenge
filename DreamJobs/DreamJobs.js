f63bcd3a37bddc1fdb24b80a25e060561038b264
Adarsh <adarsh@geekersbrain.com>
2022-10-07 23:54:54 +0530

    my first challenge

---
+++ DreamJobs/DreamJobs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DreamJobs.css'
+
+const DreamJobs = () => {
+  return (
+    <div className='dreamjobs'>
+        <h1>Your Dream Jobs Are Waiting</h1>
+        <p>Over 1 million interactions, 50,000 success stories make yours now.</p>
+        <div className=''>
+            <a href='#' className='dream-btn search-job'>Search Job</a>
+            <a href='#' className='dream-btn apply-job'>Apply Job Now</a>
+        </div>
+
+    </div>
+  )
+}
+
+export default DreamJobs
