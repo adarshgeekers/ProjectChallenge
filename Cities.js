3c3057f1e7ccb70bdd481bdb4a9274afbed7b37a
Adarsh <adarsh@geekersbrain.com>
2022-10-07 23:40:37 +0530

    my first challenge

---
+++ Cities.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Cities.css'
+import NewYork from '../../Assects/Images/cities/new york.jpg'
+import Paris from '../../Assects/Images/cities/paris.jpg'
+import Miami from '../../Assects/Images/cities/miami.jpg'
+import London from '../../Assects/Images/cities/london.jpg'
+import Losang from '../../Assects/Images/cities/losangles.jpg'
+
+
+const Cities = () => {
+  return (
+    <div className='cities'>
+        <h1>Featured Cities</h1>
+        <div className='content'>
+            <p>Lorem ipsum dolor sit amet elit, sed do eiusmod tempor</p>
+            <div className='link'>
+            <a href='#'>Browsse All Locations &#62;</a>
+            </div>
+            
+        </div>
+        <div className='images'>
+            <div className='image-left'>
+                <img src={NewYork} />
+                
+            </div>
+            <div className='image-mid'>
+                <img src={Paris} />
+                <img src={Miami} />
+
+            </div>
+            <div className='image-right'>
+                <img src={London} />
+                <img src={Losang} />
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Cities
